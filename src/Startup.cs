384f6ad4d89fe5241ffcbbd7badcbd16ddfaddf8
Ruha Bashir <ruha.bashir@stockport.gov.uk>
2020-04-29 14:59:46 +0100

    Revert "major: Upgrade to netcore 3.1 and update dependencies"
    This reverts commit 536da8f10ff9d969beed412ae21c29aca48f832c.

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,15 +1,16 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Collections.Generic;
-using access_protection_service.Utils.ServiceCollectionExtensions;
 using access_protection_service.Utils.HealthChecks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StockportGovUK.AspNetCore.Middleware;
 using StockportGovUK.AspNetCore.Availability;
 using StockportGovUK.NetStandard.Gateways;
-using Microsoft.Extensions.Hosting;
+using Swashbuckle.AspNetCore.Swagger;
+using access_protection_service.Services;
 
 namespace access_protection_service
 {
@@ -25,20 +26,35 @@ namespace access_protection_service
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddMvc().AddMvcOptions(_ => _.AllowEmptyInputInBodyModelBinding = true);
-            services.AddSwagger();
             
             services.AddHealthChecks()
                 .AddCheck<TestHealthCheck>("TestHealthCheck");                      
-            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "access_protection_service API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Authorization using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                });
+            });
+  
             services.AddAvailability();
             services.AddResilientHttpClients<IGateway, Gateway>(Configuration);
-            services.RegisterServices();
+            services.AddTransient<IAccessProtectionService, AccessProtectionService>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsEnvironment("local"))
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -47,13 +63,13 @@ namespace access_protection_service
                 app.UseHsts();
                 app.UseHttpsRedirection();
             }
-            app.UseRouting();
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
+                       
             app.UseMiddleware<ExceptionHandling>();           
             app.UseHealthChecks("/healthcheck", HealthCheckConfig.Options);
+            app.UseMvc();
             app.UseSwagger();
 
-            var swaggerPrefix = (env.IsEnvironment("local") || env.IsEnvironment("Development") ? string.Empty : "/accessprotectionservice");
+            var swaggerPrefix = (env.EnvironmentName == "local" || env.EnvironmentName == "Development") ? string.Empty : "/accessprotectionservice";
 
             app.UseSwaggerUI(c =>
             {
